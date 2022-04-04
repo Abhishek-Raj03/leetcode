ffe648737f6db51fc86e03f25c1f464513c8721a
Abhishek-Raj03 <102483394+Abhishek-Raj03@users.noreply.github.com>
2022-04-04 23:20:22 +0530

    Create 383. Ransom Note.cpp

---
+++ 383. Ransom Note.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> mp;
+        for(int i=0;i<magazine.size();i++)
+            mp[magazine[i]]++;
+       
+        for(int i=0;i<ransomNote.size();i++){
+            if(mp[ransomNote[i]]){
+                mp[ransomNote[i]]--;
+            }
+            else
+                return false;
+        }
+        return true;
+    }
+};
