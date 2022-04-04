04e44c47db67fc16e4751afa46d5a72a29b7f7c5
Abhishek-Raj03 <102483394+Abhishek-Raj03@users.noreply.github.com>
2022-04-05 00:07:51 +0530

    Create House Robber.cpp

---
+++ House Robber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+       int a=0;
+        int b=0;
+        for(int i=0;i<nums.size();i++){
+            if(i%2==0){
+                a=max(b,a+nums[i]);
+            }
+            else{
+                b=max(a,b+nums[i]);
+            }
+        }
+        return max(a,b);
+    }
+};
