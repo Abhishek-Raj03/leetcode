5672451f8412b35bf50e2290740bc452c5aed942
Abhishek-Raj03 <102483394+Abhishek-Raj03@users.noreply.github.com>
2022-04-05 19:02:12 +0530

    Create 145. Binary Tree Postorder Traversal.cpp

---
+++ 145. Binary Tree Postorder Traversal.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    
+    void postorder(TreeNode* root , vector<int> &v){
+        if(root==NULL)
+            return;
+        postorder(root->left,v);
+        postorder(root->right,v);
+        v.push_back(root->val);
+    }
+    
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> v;
+        postorder(root,v);
+        return v;
+    }
+};
